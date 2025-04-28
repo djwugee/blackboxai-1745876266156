0f017d12f5861ca8f575cde8ab5d979651d5a3ef
Kaaneii Williams <djwugee@gmail.com>
2025-04-29 09:38:52 +1200

    Add loop-drop-app/scripts/upgrade-chunks-v3.js 1745876332483

---
+++ loop-drop-app/scripts/upgrade-chunks-v3.js
@@ -0,0 +1,80 @@
+var fs = require('fs')
+var join = require('path').join
+var getDirectory = require('path').dirname
+
+var toMove = ['id', 'shape', 'flags', 'chokeAll', 'chokeGroup', 'color', 'minimised', 'routes', 'paramValues', 'volume', 'offset']
+
+upgradeProject('/Users/matt/Code/loop-drop-app/demo-project', function (err) {
+  if (err) {
+    throw err
+  } else {
+    console.log('done')
+  }
+})
+
+function upgradeProject (projectPath, cb) {
+  fs.readdir(projectPath, function (err, result) {
+    if (err) return cb && cb(err)
+    forEach(result, function (name, next) {
+      var setupPath = join(projectPath, name, 'index.json')
+      fs.exists(setupPath, function (exists) {
+        if (exists) {
+          upgradeSetup(setupPath, next)
+        } else {
+          next()
+        }
+      })
+    }, cb)
+  })
+}
+
+function upgradeSetup (setupPath, cb) {
+  console.log('upgrading', setupPath)
+  var setupDirectory = getDirectory(setupPath)
+  fs.readFile(setupPath, 'utf8', function (err, result) {
+    if (err) return cb && cb(err)
+    var data = JSON.parse(result)
+    if (data.chunks) {
+      forEach(data.chunks, function (chunk, next, i) {
+        if (chunk.node === 'external') {
+          chunk.node = 'externalChunk'
+          var chunkPath = join(setupDirectory, chunk.src)
+          console.log(' > ', chunk.src)
+          fs.readFile(chunkPath, 'utf8', function (err, result) {
+            if (err) return next && next(err)
+            var params = JSON.parse(result)
+            Object.keys(params).forEach(function (key) {
+              if (toMove.includes(key)) {
+                chunk[key] = params[key]
+                delete params[key]
+              }
+            })
+            fs.writeFile(chunkPath, JSON.stringify(params), next)
+          })
+        } else {
+          next()
+        }
+      }, function (err) {
+        if (err) return cb && cb(err)
+        fs.writeFile(setupPath, JSON.stringify(data), cb)
+      })
+    } else {
+      console.log('no chunk', setupPath)
+      cb()
+    }
+  })
+}
+
+function forEach (array, fn, cb) {
+  var i = -1
+  function next (err) {
+    if (err) return cb && cb(err)
+    i += 1
+    if (i < array.length) {
+      fn(array[i], next, i)
+    } else {
+      cb && cb(null)
+    }
+  }
+  next()
+}
